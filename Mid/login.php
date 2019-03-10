25e78f73b35a9456fcd3f39db7dd0c32a8423ddb
joehudson <47796668+joehudson@users.noreply.github.com>
2019-03-09 20:44:06 -0500

    Create login.php

---
+++ Mid/login.php
@@ -0,0 +1,33 @@
+<?php
+
+
+    $url = 'https://web.njit.edu/~hy276/beta/back/login.php';
+
+    
+    $getdata = file_get_contents('php://input');
+    
+    	
+	   // $to_back = json_decode($getdata,true);
+      
+     //$getdata = ["username" => "admin",
+       //          "password" => "teacher",
+         //        ];
+    
+ 
+   
+   $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_POST => 1,
+        CURLOPT_FOLLOWLOCATION => 1,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_POSTFIELDS => $getdata
+        ));
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    
+    echo $response;
+
+    ?>
