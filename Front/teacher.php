09226c03d782428e574e14af97a4f6659707b3fa
joehudson <47796668+joehudson@users.noreply.github.com>
2019-03-10 15:22:22 -0400

    Update teacher.php

--- Front/teacher.php
+++ Front/teacher.php
@@ -1,43 +1,47 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
 
-  $topic = $_POST['topic'];
-  $question_text = $_POST['question_text'];
-  $difficulty = $_POST['difficulty'];
-  $paramters = $_POST['parameters'];
-  $input1 = $_POST['input1'];
-  $output1 = $_POST['output1'];
-  $input2 = $_POST['input2'];
-  $output2 = $_POST['output2'];
+  if (isset($_POST['topic'])) {
+    $topic = $_POST['topic'];
+  }
+  if (isset($_POST['question_text'])) {
+    $question_text = $_POST['question_text'];
+  }
+  if (isset($_POST['difficulty'])) {
+    $difficulty = $_POST['difficulty'];
+  }
+  if (isset($_POST['parameters'])) {
+    $parameters = $_POST['parameters'];
+  }
+  if (isset($_POST['input1'])) {
+    $input1 = $_POST['input1'];
+  }
+  if (isset($_POST['output1'])) {
+    $output1 = $_POST['output1'];
+  }
+  if (isset($_POST['input2'])) {
+    $input2 = $_POST['input2'];
+  }
+  if (isset($_POST['output2'])) {
+    $output2 = $_POST['output2'];
+  }
 
   $postData = [
     "topic" => "$topic",
     "question_text" => "$question_text",
     "difficulty" => "$difficulty",
-    "parameters" => "$paramters",
+    "parameters" => "$parameters",
     "input1" => "$input1",
     "output1" => "$output1",
     "input2" => "$input2",
     "output2" => "$output2",
   ];
 
-  var_dump($postData);
-  /*
-  /*$postData = [
-    "topic" => "$_POST['topic']",
-    "question_text" => "$_POST['question_text']",
-    "difficulty" => "$_POST['difficulty']",
-    "parameters" => "$paramters",
-    "input1" => "$input1",
-    "output1" => "$output1",
-    "input2" => "$input2",
-    "output2" => "$output2"
-  ];*/
-
-$url = 'https://web.njit.edu/~jmd35/beta/createQuestion.php';
-//$url = 'https://web.njit.edu/~hy276/beta/back/createQuestion.php';
+//$url = 'https://web.njit.edu/~jmd35/beta/createQuestion.php';
+$url = 'https://web.njit.edu/~hy276/beta/back/createQuestion.php';
+//$url = 'https://web.njit.edu/~hy276/beta/front/test.php';
 
 $postData = json_encode($postData);
 
