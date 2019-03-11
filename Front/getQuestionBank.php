1c7316e19d0f68fa44dbac71f52be097358ae001
joehudson <47796668+joehudson@users.noreply.github.com>
2019-03-11 13:38:57 -0400

    Create getQuestionBank.php

---
+++ Front/getQuestionBank.php
@@ -0,0 +1,28 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//$url = 'https://web.njit.edu/~jmd35/beta/getQuestionBank.php'; //change to this DIR
+$url = 'https://web.njit.edu/~hy276/beta/back/getQuestionBank.php'; //test DIR
+$opts = [
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => 1,
+  CURLOPT_FOLLOWLOCATION => 1
+];
+
+$ch = curl_init();
+
+curl_setopt_array($ch, $opts);
+
+$result = curl_exec($ch);
+
+if (!$result) {
+  die('connection failed');
+}
+
+curl_close($ch);
+
+echo $result;
+
+ ?>
