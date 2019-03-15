3c06c1645a27d8cd50a702f1f3dbeabdf2a638c3
joehudson <47796668+joehudson@users.noreply.github.com>
2019-03-15 14:33:14 -0400

    Update showQuestionTblChecks.php

--- Front/showQuestionTblChecks.php
+++ Front/showQuestionTblChecks.php
@@ -3,8 +3,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//$url = 'https://web.njit.edu/~jmd35/beta/getQuestionBank.php'; //change to this DIR
-$url = 'https://web.njit.edu/~hy276/beta/back/showQuestionTblChecks.php'; //test DIR
+$url = 'https://web.njit.edu/~jmd35/beta/getQuestionBank.php'; //change to this DIR
+//$url = 'https://web.njit.edu/~hy276/beta/back/showQuestionTblChecks.php'; //test DIR
 $opts = [
   CURLOPT_URL => $url,
   CURLOPT_RETURNTRANSFER => 1,
