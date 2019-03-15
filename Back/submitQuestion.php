d95762324d8e49cd3f7579ddb6bbc4ac071f4fe6
joehudson <47796668+joehudson@users.noreply.github.com>
2019-03-15 12:59:00 -0400

    Update submitQuestion.php

--- Back/submitQuestion.php
+++ Back/submitQuestion.php
@@ -1,71 +1,82 @@
 <?php
-//back
-error_reporting(E_ALL);
-
-$getData = array();
-
-$array = file_get_contents("php://input");
-
-var_dump($array); //array is just NULL here, so I cant do anything with the input
-
-$getData = json_decode($array, true);
-
-
-if (!isset($test)) {
-  $test = new stdClass();
-}
-
-$test->response = $getData;
-$test->msg = "question insert failed";
-$test = json_encode($test);
-
-echo $test;
-
-/*
-$dbhost = "sql1.njit.edu";
-$dbuser = "hy276";
-$dbpass = "HY9Co7Qkq";
-$dbname = "hy276";
-
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-if (!$conn) {
-  die("Cannot connect to DB: " . mysqli_connect_error());
-}
-
-$itWorked = false;
-$length = count($getData);
-
-for ($i = 1; $i < $length; $i+=6) {
-  foreach ($getData as $item) {
-    $id = $items[i];
-
-    $query = "INSERT INTO `exams` (`question_id`) VALUES ('$id')";
-
-    if ($response = mysqli_query($conn, $query)) {
-        $GLOBALS['itWorked'] = true;
-    }
-    else {
-      die("query failed");
-    }
+ 
+ //mysql credentials
+  $dbhost = "sql1.njit.edu";
+  $dbuser = "hy276";
+  $dbpass = "HY9Co7Qkq";
+  $dbname = "hy276";
+  //login to
+  
+  
+  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+  
+  if (!$conn) {
+    die("Cannot connect to DB: " . mysqli_connect_error());
   }
-}
+  
+  $getdata = file_get_contents('php://input');
+  
+  
+  $array  = json_decode($getdata,true);
+  
+  /*$array = ["qid1" => "22",
+            "qid2" => "33",
+            "qid3" => "44",
+            "exam_name" => "Test",
+            "points1" => "25",
+            "points2" => "25",
+            "points3" => "50"
+               ];
+  */
+  
+  
+  $question_id1    = $array['qid1'];
+  $question_id2     = $array['qid2'];
+  $question_id3     = $array['qid3'];
+  $exam_name       = $array['exam_name'];
+  $points1           = $array['points1'];
+  $points2         = $array['points2'];
+  $points3           = $array['points3'];
+  
+  
 
-if (!isset($query_json)) {
-  $query_json = new stdClass();
-}
-if ($itWorked) {
-  $query_json->msg = "question insert ok";
-}
-if ($itWorked === false) {
-  $query_json->response = $getData;
-  $query_json->msg = "question insert failed";
-}
-
-$query_json = json_encode($query_json);
-
-echo $query_json;
-
-mysqli_close($conn);
-
-?>
+  $query1 = "INSERT INTO `exams`(`exam_name`, `question_id`, `score`) VALUES ('$exam_name','$question_id1','$points1')";
+  $query2 = "INSERT INTO `exams`(`exam_name`, `question_id`, `score`) VALUES ('$exam_name','$question_id2','$points2')";
+  $query3 = "INSERT INTO `exams`(`exam_name`, `question_id`, `score`) VALUES ('$exam_name','$question_id3','$points3')";
+  
+   $itWorked = false;
+  
+  if ($response = mysqli_query($conn, $query1)) { //checks if the query worked
+      //echo "<script>console.log('query worked')</script>";
+      $itWorked = true;
+  }
+  
+   if ($response = mysqli_query($conn, $query2)) { //checks if the query worked
+      //echo "<script>console.log('query worked')</script>";
+      $itWorked = true;
+  }
+   if ($response = mysqli_query($conn, $query3)) { //checks if the query worked
+      //echo "<script>console.log('query worked')</script>";
+      $itWorked = true;
+  }
+  //else {
+    //  echo "<script>console.log('Something went wrong')</script>";
+    //}
+   //creating json to send back
+   
+  if (!isset($login_json)) {
+    $login_json = new stdClass();
+  }
+  if ($itWorked) {
+    
+    $login_json->msg = "exam added";
+  }
+  
+  else if ($itWorked === false) {
+   
+    $login_json->msg = "not added";
+  }
+  $login_json = json_encode($login_json, true);
+  echo $login_json;
+  
+  mysqli_close($conn);
