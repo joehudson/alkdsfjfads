436b0e46d332b3cc00708e22884cd8658367c07b
joehudson <47796668+joehudson@users.noreply.github.com>
2019-03-18 11:07:50 -0400

    Update submitQuestion.php

--- Front/submitQuestion.php
+++ Front/submitQuestion.php
@@ -5,21 +5,51 @@ ini_set('display_errors', 1);
 
 //needs to be directed to mid
 //$url = 'http://web.njit.edu/~hy276/beta/back/submitQuestion.php';
-//$url = 'http://web.njit.edu/~hy276/beta/back/test.php';
-$url = 'http://web.njit.edu/~jmd35/beta/mid/submitQuestion.php';
+$url = 'http://web.njit.edu/~jmd35/beta/submitQuestion.php';
 
-$getData = $_POST;
+if (isset($_POST['exam_name'])) {
+  $exam_name = $_POST['exam_name'];
+}
+if (isset($_POST['qid1'])) {
+  $qid1 = $_POST['qid1'];
+}
+if (isset($_POST['points1'])) {
+  $points1 = $_POST['points1'];
+}
+if (isset($_POST['qid2'])) {
+  $qid2 = $_POST['qid2'];
+}
+if (isset($_POST['points2'])) {
+  $points2 = $_POST['points2'];
+}
+if (isset($_POST['qid3'])) {
+  $qid3 = $_POST['qid3'];
+}
+if (isset($_POST['points3'])) {
+  $points3 = $_POST['points3'];
+}
 
-$send = json_encode($getData, true);
+$post = [
+  "exam_name" => "$exam_name",
+  "qid1" => "$qid1",
+  "points1" => "$points1",
+  "qid2" => "$qid2",
+  "point2" => "$points2",
+  "qid3" => "$qid3",
+  "points3" => "$points3",
+];
 
-//var_dump($send);
+$send = json_encode($post, true);
+
+//`echo $send;
 
 $opts = [
   CURLOPT_URL => $url,
   CURLOPT_RETURNTRANSFER => 1,
   CURLOPT_FOLLOWLOCATION => 1,
   CURLOPT_POST => 1,
-  CURLOPT_POSTFIELDS => $send,
+  CURLOPT_POSTFIELDS => $send
+
 ];
 
 $ch = curl_init();
